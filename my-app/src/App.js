280776053bdc55c7610b0a7c1350f0ab0286f167
Adrian Bunge <adrian@nonacreative.com>
2018-06-08 12:05:27 +0200

    All components hosted in the same file - App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,105 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './nona_logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.inputRef = React.createRef();
+    this.state = {
+      todos: [
+        {id : this.generateUniqueId(), todo : 'Learn React', status: 'To do'},
+        {id : this.generateUniqueId(), todo : 'Learn Redux', status: 'To do'},
+        {id : this.generateUniqueId(), todo : 'Learn CSS Flex', status: 'To do'}
+      ],
+      inputValue: ''
+    }
+  }
+
+  updateInputValue = (event) => {
+    event.preventDefault()
+    let text = event.target.value
+    this.setState({inputValue: text})
+  }
+
+  deleteItem = (id) => {
+    let todos = [...this.state.todos]
+    todos = todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({todos: todos})
+  }
+  
+  generateUniqueId = () => {
+    return Math.random().toString(36).substr(2, 9)
+  }
+  
+  addItem = () => {
+    let todos = [...this.state.todos, {
+      id: this.generateUniqueId(),
+      todo: this.inputRef.current.value,
+      status: 'To do'
+    }]
+    this.setState({todos: todos})
+  }
+
+  moveItem = (id, status) => {
+    let todos = [...this.state.todos]
+    let desiredIndex = todos.findIndex(todo => todo.id === id)
+    switch (status) {
+      case 'To do':
+        todos[desiredIndex].status = 'In progress'
+      break
+      case 'In progress':
+        todos[desiredIndex].status = 'Done'
+      break
+    }
+    this.setState({todos: todos})
+  }
+
   render() {
+    const ToDoItem = (props) => {
+      return <div className = "ToDoItem" >
+              <p> {props.todo.todo}</p>
+              <div className = "Buttons">
+                <button onClick={(event) => this.deleteItem(props.todo.id)} className = "Delete"> Delete </button>
+                {props.todo.status === 'Done' ? '' : <button onClick={(event)=>this.moveItem(props.todo.id, props.todo.status)} className = "Next"> Next </button>}
+              </div>
+          </div> 
+  }
+
+  const ToDoList = (props) => {
+    return <div className = "Status-Container"><h1> {props.heading}</h1>
+    {props.heading === 'To do' ? 
+      <div> 
+        <input ref={this.inputRef} placeholder="Type your to do here"/><button onClick={() => this.addItem()} className = "AddItem"> Add item </button> 
+      </div>: ''
+    }
+    {props.todos.map((todo) => {
+      return props.heading === todo.status ? <ToDoItem
+      key = {todo.id}
+      todo = {todo}  
+      /> : ''
+    })}</div>
+}
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Nello (The Nona - Trello)</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className = "Content">
+          <ToDoList todos = {this.state.todos} heading = "To do" />
+          <ToDoList todos = {this.state.todos} heading = "In progress"  />
+          <ToDoList todos = {this.state.todos} heading = "Done"  />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+
+
