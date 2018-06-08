7579866ee5b8f8be98b72ebab3f499ce4824c870
Adrian Bunge <adrian@nonacreative.com>
2018-06-08 12:23:44 +0200

    Implement CSS and Logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './nona_logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Nello (The Nona - Trello)</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
