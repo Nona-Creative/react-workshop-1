20ecf52a242d6a033b95a3013a638f9d8252169b
Adrian Bunge <adrian@nonacreative.com>
2018-06-08 12:43:32 +0200

    Add props to react components. Move components (JSX) from the render and give them file-scope

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,31 @@ import React, { Component } from 'react';
 import logo from './nona_logo.svg';
 import './App.css';
 
+// Defining a react component that takes props (properties) as a parameter
+// Components should be declared above the class instead on in the render method
+// to give them file-scope. If left in the render method the component will be 
+// recreated everytime the render method is called which is inefficient.
+const ToDoItem = (props) => {
+  return <div className = "ToDoItem" >
+          <p> {props.todo}</p>
+        </div> 
+}
+
 class App extends Component {
   render() {
-    // Defining a react component that takes no parameters 
-    // Arrow Functions must always have a defined return
-    const ToDoItem = () => {
-      return <div className = "ToDoItem" >
-              <p> Learn React</p>
-          </div> 
-    }
-
+    
     return (
-      // Note that JSX looks similar to HTML but it isn't exactly the same. Html -> class="foo" JSX className="bar"
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Nello (The Nona - Trello)</h1>
         </header>
-        {/* <div> added for styling purpose - otherwise the components will display under the header which is -> position: fixed; */}
         <div className = "Content">
         {/* Calling the react component to replicate itself 3 times */}
-          <ToDoItem/>
-          <ToDoItem/>
-          <ToDoItem/>
+        {/* The props.todo in each instance of the component is sent to the given string */}
+          <ToDoItem todo = 'Learn React'/>
+          <ToDoItem todo = 'Learn CSS Flex'/>
+          <ToDoItem todo = 'Learn Redux'/>
         </div>
       </div>
     );
