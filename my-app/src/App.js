4cb38bf08f89108c0bc059a9d829fe9036365d9e
Adrian Bunge <adrian@nonacreative.com>
2018-06-09 09:23:42 +0200

    Implement a map function to avoid hard coding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import logo from './nona_logo.svg';
 import './App.css';
 
-// Defining a react component that takes props (properties) as a parameter
-// Components should be declared above the class instead on in the render method
-// to give them file-scope. If left in the render method the component will be 
-// recreated everytime the render method is called which is inefficient.
 const ToDoItem = (props) => {
   return <div className = "ToDoItem" >
           <p> {props.todo}</p>
         </div> 
 }
 
+const ToDoList = (props) => {
+  return <div className = "Status-Container">{
+    props.todos.map((todo) => { // Map through the todos array from the state
+      console.log(todo)
+      return<ToDoItem
+        key = {todo.id}  // A unique key is required so that react knows how to target specific views that have changed in the event of rerender
+        todo = {todo.todo}  
+        /> 
+      })}
+    </div>
+}
+    
 class App extends Component {
-// State is initialised once in the constructor using this.state = {...}. If state is changed later setState must be used
-// We set state to an array of todo elements
   constructor(props){
     super(props)
     this.state = {
@@ -22,7 +28,7 @@ class App extends Component {
         {id : '1', todo : 'Learn React'},
         {id : '2', todo : 'Learn Redux'},
         {id : '3', todo : 'Learn CSS Flex'}
-        // id isn't used in the step but fear not it will be in the next one #HakunaMatata
+     
       ]
     }
   }
@@ -35,13 +41,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to Nello (The Nona - Trello)</h1>
         </header>
         <div className = "Content">
-        {/* select element i from the state array */}
-        {/* This may seem verbose... because it is... 
-            we need some sort of loop...
-            but which one? The plot thickens (although the code should become thinner) */}
-          <ToDoItem todo = {this.state.todos[0].todo}/>
-          <ToDoItem todo = {this.state.todos[1].todo}/>
-          <ToDoItem todo = {this.state.todos[2].todo}/>
+          <ToDoList todos = {this.state.todos} /> 
+          {/* Take a moment... to appreciate how deeply pleasing this is compared the hard 
+          coded grossness we previously had */}
         </div>
       </div>
     );
